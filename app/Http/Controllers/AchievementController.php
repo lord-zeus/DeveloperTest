f906e4cf44509c227d18f87c654b99781af4b155
Xeus <nsaghakingsly@gmail.com>
2022-01-29 02:10:55 +0100

    want to conclude with remaining to unluck next...

--- app/Http/Controllers/AchievementController.php
+++ app/Http/Controllers/AchievementController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Achievement;
+use App\Models\Badge;
+use App\Models\User;
 use App\Traits\APIResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class AchievementController extends Controller
 {
@@ -31,5 +36,32 @@ class AchievementController extends Controller
         ]);
         $achievement = Achievement::create($request->all());
         return $this->successResponse($achievement);
+    }
+
+    public function unlockedAchievements($user_id){
+        $user = User::findOrFail($user_id);
+        $achievements = $user->achievements->sortBy('number');
+        $comment =  $achievements->where('type', 'comment')->last();
+        $lesson = $achievements->where('type', 'lesson')->last();
+        if(empty($comment)){
+            $all_achievement_comment = Achievement::where('type', 'comment')->orderBy('number')->first();
+        }
+        if(empty($lesson)){
+            $all_achievement_lesson = Achievement::where('type', 'lesson')->orderBy('number')->first();
+        }
+
+        $all_achievement_lesson = Achievement::where('type', 'lesson')->where('number', '>', $lesson->number)->orderBy('number')->first();
+        $all_achievement_comment = Achievement::where('type', 'comment')->where('number', '>', $comment->number)->orderBy('number')->first();
+
+        $user_badge = $user->badges->sortBy('number_achievements')->last();
+        $next = Badge::where('number_achievements', '>', $user_badge->number_achievements)->orderBy('number_achievements')->first();
+        $final = [
+            'unlocked_achievements' => $achievements->pluck('name'),
+            'next_available_achievements' => [$all_achievement_lesson->name, $all_achievement_comment->name],
+            'current_badge' => $user_badge->name,
+            'next_badge' => $next->name,
+            'remaining_to_unlock_next_badge' =>( (int)$next->number_achievements - (int)$user_badge->number_achievements)
+        ];
+        return $this->successResponse($final);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AchievementsController;
+use App\Http\Controllers\AchievementController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/users/{user}/achievements', [AchievementsController::class, 'index']);
+Route::get('/users/{user}/achievements', [AchievementController::class, 'unlockedAchievements']);
 
-Route::group([], function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-});
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
